97216ce1137834dd219e5399e274347076ae9795
Bivek Basnet <your-email@example.com>
2025-05-11 12:43:17 +0545

    Add post creation form to admin panel

--- resources/views/admin/post.blade.php
+++ resources/views/admin/post.blade.php
@@ -25,8 +25,34 @@
       <!-- Sidebar Navigation-->
         @include('admin.sidebar')
       <!-- Sidebar Navigation end-->
-      
+        <div class="page-content">
+            <p style="text-align: center;color:white;font-size:60px">Add Post</p>
+            <form action="{{ url('add_post') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @if(session()->has('message'))
+                <div class="alert alert-danger">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x</button>
+                    {{ session()->get('message') }}
+                </div>
+                @endif
+                <div class="center">
+                    <label>Post Title</label>
+                    <input type="text" name="title" placeholder="Write a title" required="">
+                </div>
+                <div class="center">
+                    <label>Description</label>
+                    <textarea name="description" placeholder="Write a description" required=""></textarea>
+                </div>
+                <div class="center">
+                    <label>Image</label>
+                    <input type="file" name="image" required="">
+                </div>
+                <div class="center">
+                    <button class="btn">Submit</button>
+                </div>
+            </form>
     <!-- JavaScript files-->
+
         @include('admin.javascript')
   </body>
 </html>
